b8dedf0d49ebc3168d3844169cacf3f9040bf6c9
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2021-04-17 14:13:32 -0700

    week 3: flow chart tool with drag and droppp shapes

---
+++ FlowChartTool/Assets/Scripts/UIElementDragger.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIElementDragger : MonoBehaviour
+{
+    public GameObject FlowChartPanel;
+
+    public const string DRAGGABLE_TAG = "UIDraggable";
+
+    private bool dragging = false;
+
+    private Vector2 originalPosition;
+
+    private Transform objectToDrag;
+    private Image objectToDragImage;
+
+    List<RaycastResult> hitObjects = new List<RaycastResult>();
+
+    #region MonoBehavior API
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 0 = left mouse button
+        {
+            objectToDrag = GetDraggableTransformUnderMouse();
+
+            if(objectToDrag != null)
+            {
+                dragging = true;
+
+                objectToDrag.SetAsLastSibling();
+
+                originalPosition = objectToDrag.position;
+            }
+        }
+
+        if (dragging)
+        {
+            objectToDrag.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+
+            if (objectToDrag != null)
+            {
+                Image clonedObject = Instantiate(objectToDrag.GetComponent<Image>());
+                //clonedObject.tag = "Untagged";
+                clonedObject.raycastTarget = false;
+                clonedObject.transform.SetParent(FlowChartPanel.transform);
+
+                clonedObject.transform.position = Input.mousePosition;
+                objectToDrag.position = originalPosition;
+            }
+        }
+    }
+
+    #endregion
+
+    private GameObject GetObjectUnderMouse()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+
+        pointer.position = Input.mousePosition;
+
+        EventSystem.current.RaycastAll(pointer, hitObjects);
+
+        if (hitObjects.Count <= 0)
+        {
+            return null;
+        }
+        return hitObjects[0].gameObject;
+    }
+
+    private Transform GetDraggableTransformUnderMouse()
+    {
+        GameObject clickedObject = GetObjectUnderMouse();
+
+        if (clickedObject != null && clickedObject.tag == DRAGGABLE_TAG)
+        {
+            return clickedObject.transform;
+        }
+        return null;
+    }
+}
